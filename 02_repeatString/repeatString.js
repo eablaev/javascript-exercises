bc37aed3a729e60a1cd236072473bbee26f33b79
shmerv <eaudioworks@gmail.com>
2022-09-20 09:34:03 -0700

    Comleted 02_RepeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
 
+
+let result ="";
+
+const repeatString = function(string, number) {
+
+    
+for( let i = 0; i < number; i ++){
+    result = result+=string;
+    //console.log("thisis a loop "+result)
+    
+}
+return result;
 };
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
