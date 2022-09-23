b5215f3e02812fa80b132689f8ff7a8932a24f48
shmerv <eaudioworks@gmail.com>
2022-09-23 12:04:05 -0700

    Completed 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,46 @@
 const sumAll = function() {
 
+
+
+let startCount = arguments[0]
+let endCount = arguments[1];
+let allNumbers = [];
+let currentNumber = 0;
+
+
+
+ if(startCount < 0 || endCount <0 || typeof startCount !== "number" || typeof endCount !== "number"){
+    return 'ERROR';
+}
+
+    else if(startCount <= endCount && startCount > 0 && endCount >0){
+        for(let i = 0; i < endCount; i++){
+            currentNumber ++;
+             allNumbers.push(currentNumber);}
+       
+    }
+
+    else if(startCount > endCount  && startCount > 0 && endCount >0){
+        for(let i = 0; i < startCount; i++){
+            currentNumber ++;
+             allNumbers.push(currentNumber);}
+
+    }
+
+   
+
+
+    allNumbers = allNumbers.reduce((accumulator, currentVal)=> accumulator + currentVal, 0)
+    
+    
+
+
+return allNumbers;
+
 };
+
+//npm test sumAll.spec.js
+
 
 // Do not edit below this line
 module.exports = sumAll;
