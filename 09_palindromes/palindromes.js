d07ab65874cbbdb1fec23fad951ce274f03ea9c8
shmerv <eaudioworks@gmail.com>
2022-10-05 11:42:18 -0700

    Completed palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,25 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const string = str.split('')
+                        .join('')
+                        .split(/[''.,!\n\s]/)
+                        .join('')
+                        .toLowerCase();
+    const revString = str.split('')
+                        .reverse()
+                        .join('')
+                        .split(/[''.,!\n\s]/)
+                        .join('')
+                        .toLowerCase();
+console.log(string+" "+revString)
+
+    return string === revString ? true : false
+
 
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+
+
+
+//npm test palindromes.spec.js
