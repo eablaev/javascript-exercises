7b7b6303ac07798726a96d2bafa4ebf5ef6c93a1
shmerv <eaudioworks@gmail.com>
2022-10-05 15:57:48 -0700

    Completed fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,34 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
 
-};
+    let a = 0;
+    let b = 1;
+    let c = 0;
+
+    let n = Number(num);
+    //console.log(typeof(n));
+
+    if( n>0){
+        for(let i= 1; i<n; i++){
+        
+            c = a+b;//c = 0+1 = 1
+            a = b;//a = 1
+            b =c; // b = 1
+ }
+ //console.log(c);
+    return b;
+}
+
+    else if(n < 0){
+        return "OOPS";
+    }
+
+    
+    }
+    
+;
 
 // Do not edit below this line
 module.exports = fibonacci;
+
+
+//npm test fibonacci.spec.js
