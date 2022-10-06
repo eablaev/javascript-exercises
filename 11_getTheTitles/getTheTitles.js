15070227b9c2d82d0bc963d1e4d0a817343637d2
shmerv <eaudioworks@gmail.com>
2022-10-05 20:38:18 -0700

    Completed get the titles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,19 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
 
+    let titles = [];
+
+books.forEach( book => {
+    console.log(book.title);
+    titles.push(book.title);
+    
+    
+});
+return titles
 };
 
 // Do not edit below this line
 module.exports = getTheTitles;
+
+
+//npm test getTheTitles.spec.js
+
